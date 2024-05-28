1e95895dfd7e764b0e06c65a117ae2c9432853bb
Galang Dwi Juniar <galang@Galang.local>
2024-05-28 19:15:54 +0700

    back to listing

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -1,23 +1,23 @@
-nokogiri = Nokogiri.HTML(content)
-products = nokogiri.css('.s-latency-cf-section .s-result-item .sg-col-inner')
-puts nokogiri
-# products = nokogiri.css('#mainResults li', '#resultsCol li')
-products.each do |product|
-  a_element = product.at_css('[data-cy="title-recipe"] a.a-link-normal.a-text-normal')
-  if a_element
-    url = a_element['href'].gsub(/&qid=[0-9]*/,'')
-    if url =~ /\Ahttps?:\/\//i
-      pages << {
-          url: url,
-          page_type: 'products',
-          vars: {
-            category: page['vars']['category'],
-            url: url
-          }
-        }
-    end
-  end
-end
+# nokogiri = Nokogiri.HTML(content)
+# products = nokogiri.css('.s-latency-cf-section .s-result-item .sg-col-inner')
+# # puts nokogiri
+# # products = nokogiri.css('#mainResults li', '#resultsCol li')
+# products.each do |product|
+#   a_element = product.at_css('[data-cy="title-recipe"] a.a-link-normal.a-text-normal')
+#   if a_element
+#     url = a_element['href'].gsub(/&qid=[0-9]*/,'')
+#     if url =~ /\Ahttps?:\/\//i
+#       pages << {
+#           url: url,
+#           page_type: 'products',
+#           vars: {
+#             category: page['vars']['category'],
+#             url: url
+#           }
+#         }
+#     end
+#   end
+# end
 
 # total_page_summary = nokogiri.css("li.comet-pagination-item.comet-pagination-item-60 > a").text
 # total_page = total_page_summary.scan(/\d+/).first.to_i
