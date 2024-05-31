31e9e0384716214cc29dba76ca0e8a8564286142
Galang Dwi Juniar <galang@Galang.local>
2024-05-31 15:36:10 +0700

    first

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -19,3 +19,13 @@ pages << {
     category: "OLED TVs"
   }
 }
+pages << {
+  page_type: 'listings',
+  method: "GET",
+  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+  # url: "https://www.amazon.com/s/ref=lp_172659_nr_n_1?fst=as%3Aoff&rh=n%3A172282%2Cn%3A%21493964%2Cn%3A1266092011%2Cn%3A172659%2Cn%3A6463520011&bbn=172659&ie=UTF8&qid=1547749731&rnid=172659",
+  url: "https://www.amazon.com/s?k=oled+tv%27s&rh=n%3A1266092011&ref=nb_sb_noss",
+  vars: {
+    category: "2. OLED TVs"
+  }
+}
